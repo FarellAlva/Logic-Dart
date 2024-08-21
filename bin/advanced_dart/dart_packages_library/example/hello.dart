ad1438f1017b2163f36937e70c989b7310a0811b
FarellAlva <farell6646@gmail.com>
2024-08-21 18:54:25 +0700

    library

---
+++ bin/advanced_dart/dart_packages_library/example/hello.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:dart_packages_library/hello.dart';
+
+void main() {
+  print(sayHello('Eko'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
---
+++ bin/advanced_dart/dart_packages_library/lib/hello.dart
@@ -0,0 +1,4 @@
+library dart_packages_library_base.dart;
+
+export 'src/say_hello.dart' show sayHello;
+export 'src/customer.dart' show Category, Customer, Product;
