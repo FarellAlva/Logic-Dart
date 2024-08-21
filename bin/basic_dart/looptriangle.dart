7432baf386a98ab4c5ce9810a8e589467a5db08e
FarellAlva <farell6646@gmail.com>
2024-08-21 18:06:00 +0700

    edit on basic

--- bin/basic_dart/looptriangle.dart
+++ bin/basic_dart/looptriangle.dart
@@ -3,14 +3,18 @@ import 'dart:io';
 void main() {
   int i = 0;
   int row;
+  int j = 0;
 
   stdout.write('how many row of star u want?');
   row = int.parse(stdin.readLineSync()!);
 
-  for (i = 0; i < row; i++) {
-    for (int j = 0; j <= i; j++) {
+  for (i = 1; i <= row; i++) {
+    for (j = 1; j <= i; j++) {
       stdout.write('* $j');
     }
     stdout.writeln();
   }
+
+  print(j);
+  print(i);
 }
