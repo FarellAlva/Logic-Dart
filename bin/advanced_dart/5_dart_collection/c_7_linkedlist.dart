5622c2dd6cd5e49051e71569251094d44554890c
FarellAlva <farell6646@gmail.com>
2024-08-23 19:32:42 +0700

    updated

--- bin/advanced_dart/5_dart_collection/c_7_linkedlist.dart
+++ bin/advanced_dart/5_dart_collection/c_7_linkedlist.dart
@@ -2,10 +2,21 @@ import 'dart:collection';
 
 //harus menentukan terlebih jenis kelas untuk pewarisan tunggal
 //base, final, sealed
-sealed class StringEntry extends LinkedListEntry<StringEntry> {
+
+//(base itu bisa dipakai dimana saja)
+//(final itu tidak bisa lagi dipakai setelah ini)
+//(sealed itu hanya bisa dipakai di modul/paket yang sama)
+base class StringEntry extends LinkedListEntry<StringEntry> {
   String value;
 
   StringEntry(this.value);
 }
 
-void main() {}
+void main() {
+  final linkedlist = LinkedList<StringEntry>();
+  linkedlist.addAll([StringEntry('Farell'), StringEntry('Alvaro')]);
+
+  for (var entry in linkedlist) {
+    print(entry.value);
+  }
+}
