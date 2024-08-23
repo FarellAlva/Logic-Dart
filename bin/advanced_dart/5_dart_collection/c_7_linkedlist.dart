6f6072a78859f6ddb7b0389f928daebde0c301a8
FarellAlva <farell6646@gmail.com>
2024-08-23 18:39:52 +0700

    updated

--- bin/advanced_dart/5_dart_collection/c_7_linkedlist.dart
+++ bin/advanced_dart/5_dart_collection/c_7_linkedlist.dart
@@ -1,6 +1,8 @@
 import 'dart:collection';
 
-class StringEntry extends LinkedListEntry<StringEntry> {
+//harus menentukan terlebih jenis kelas untuk pewarisan tunggal
+//base, final, sealed
+sealed class StringEntry extends LinkedListEntry<StringEntry> {
   String value;
 
   StringEntry(this.value);
