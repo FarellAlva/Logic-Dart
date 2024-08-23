b6b8ec131adcbd932c0c6ced2b41ee738d05adf4
FarellAlva <farell6646@gmail.com>
2024-08-23 22:49:55 +0700

    modify linkedset list

--- bin/advanced_dart/5_dart_collection/c_9_linkedHashSet.dart
+++ bin/advanced_dart/5_dart_collection/c_9_linkedHashSet.dart
@@ -23,3 +23,78 @@ void main() {
 
   print(set);
 }
+
+class Node<T> {
+  T data;
+  Node<T>? next;
+  Node<T>? prev;
+
+  Node(this.data);
+}
+
+//IMPLEMENTASI SESUNGGUHNYA dengan double linked list didart
+// class DoublyLinkedList<T> {
+//   Node<T>? head;
+//   Node<T>? tail;
+
+//   void append(T data) {
+//     final newNode = Node<T>(data);
+
+//     if (head == null) {
+//       head = tail = newNode;
+//     } else {
+//       tail?.next = newNode;
+//       newNode.prev = tail;
+//       tail = newNode;
+//     }
+//   }
+
+//   void prepend(T data) {
+//     final newNode = Node<T>(data);
+
+//     if (head == null) {
+//       head = tail = newNode;
+//     } else {
+//       newNode.next = head;
+//       head?.prev = newNode;
+//       head = newNode;
+//     }
+//   }
+
+//   void displayForward() {
+//     Node<T>? current = head;
+
+//     while (current != null) {
+//       print(current.data);
+//       current = current.next;
+//     }
+//   }
+
+//   void displayBackward() {
+//     Node<T>? current = tail;
+
+//     while (current != null) {
+//       print(current.data);
+//       current = current.prev;
+//     }
+//   }
+// }
+
+// void main() {
+//   final dll = DoublyLinkedList<int>();
+
+//   dll.append(1);
+//   dll.append(2);
+//   dll.append(3);
+
+//   print('Display Forward:');
+//   dll.displayForward(); // Output: 1 2 3
+
+//   print('Display Backward:');
+//   dll.displayBackward(); // Output: 3 2 1
+
+//   dll.prepend(0);
+  
+//   print('Display Forward after prepending:');
+//   dll.displayForward(); // Output: 0 1 2 3
+// }
