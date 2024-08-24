16a99b4249c91e9c326726aecea51f531adeec38
FarellAlva <farell6646@gmail.com>
2024-08-24 14:19:06 +0700

    transform method

---
+++ bin/advanced_dart/5_dart_collection/c_24_transform_method.dart
@@ -0,0 +1,9 @@
+//transform method
+//iterbale juga memiliki method untuk melakukan transform untuk datanya
+
+//method ------------------------------------------- keterangan
+//expand(Iterable<T>toElements(E)): Iterable<t>      mengubah tiap element menjadi iterable<T> dan menggabungkan semuanya
+
+//map(T toElement(E)) : Iterable<T>                  mengubah tiap element mengguanakn function toElement
+
+//join(separator) : String                           mengubah element menjadi string, lalu menggabungkan dengan separator
