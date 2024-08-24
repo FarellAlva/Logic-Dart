4eefaf9b851d072875d46e58f3692f120f7c8343
FarellAlva <farell6646@gmail.com>
2024-08-24 07:30:24 +0700

    splay treeset

---
+++ bin/advanced_dart/5_dart_collection/c_11_splay_tree_set.dart
@@ -0,0 +1,12 @@
+//splay tree set merupakan implementasi dari Set yang menggunakan struktur data Tree
+//hal ini menyebabkan data di Splay tree set akan secara otomatis berurut sesuai comparatornya, defaultnya adalah ascneding
+
+import 'dart:collection';
+
+void main() {
+  final treeSet = SplayTreeSet<dynamic>();
+
+  treeSet.addAll([1, 2, 4, 2, 5, 3, 5]);
+
+  print(treeSet);
+}
